99d9f6bd0c39a9f6a58f4a4853b91395ca934239
Padawan04 <Padawan04@DESKTOP-LV5RKMD>
2020-03-26 12:39:44 +0100

    Sistemazione codice, funzionante anche la visualizzazione utente (con id cablato nel codice)

--- React/utenti/src/UserContainer.js
+++ React/utenti/src/UserContainer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//
+// OK ?
 class UserContainer extends React.Component {	
 
 	state = {
@@ -12,12 +12,54 @@ class UserContainer extends React.Component {
 	render() {
 		return (
 			<div>
-				<Left name="left" newState={this.handleClick} />
-				<Right name="right" value={this.state.id || "-1"} />
+				<Left name="left" />
+				<Right name="right" value={this.state.id || "1"} />
 			</div>
 		);
 	}
 }
+
+class Right extends React.Component {
+	
+	state = {
+		user: {},
+		id: null
+	}
+	
+
+	componentWillMount() {
+		if(this.state.id) {
+			fetch('https://jsonplaceholder.typicode.com/users/' + this.state.id)
+				.then(response => response.json())
+				.then(json => {
+					this.setState({user: json });
+					console.log("JSON: " + json);
+					this.setState({user: json});
+				});
+		}
+	}
+	
+		
+	render() {
+		return(
+			<div>Id: {this.state.user.id} - Nome: {this.state.user.name}</div>
+		);
+
+	}
+}
+	
+
+class DettaglioUtente extends React.Component {
+	render() {
+		return(
+			<div>{this.props.user}</div>
+		);
+	}
+}
+
+
+
+
 
 // OK
 class Left extends React.Component {
@@ -54,77 +96,26 @@ class Left extends React.Component {
 }
 
 
-class Right extends React.Component {
-	
-	render() {
-		if(this.props.value === "-1") {
-			return(
-				<div className={this.props.name}>{this.props.value}</div>
-			);
-		} else {
-			return(
-				<div className={this.props.name}>div right</div>
-			);
-		}
-	}
-}
-	
-
-class DettaglioUtente extends React.Component {
-	render() {
-		return(
-			<div>{this.props.user}</div>
-		);
-	}
-}
-
+// OK
 class UserItem extends React.Component {
 	
 	constructor(props) {
 		super(props);
 		this.mostraUser = this.mostraUser.bind(this);
 	}
-	
-	state = {
-		user: {},
-		id: ""
-	}
-	
-	handleClick = idButton => {
-		this.props.clickHandler(idButton);
-		console.log(idButton);
-	};
-	
-	componentWillMount() {
-		fetch('https://jsonplaceholder.typicode.com/users/' + this.props.id)
-			.then(response => response.json())
-			.then(json => {
-				this.setState({user: json });
-				console.log("JSON: " + json);
-				//console.log(this.state.users);
-			});
-	}
 			
 	mostraUser(event) {
-		console.log("MostraUser: " + this.state.user.id);
+		console.log("MostraUser: " + this.props.id);
 	}
 	
 	render () {
 		return(
 			<div>
-				<button type="button" id={this.props.id} onClick={this.mostraUser}>Id: {this.props.id}, Name: {this.props.name}</button>
+				<button type="button" id={this.props.id} onClick={this.mostraUser}>Id: {this.props.id}, Name: {this.props.name}, prova: {this.props.id}</button>
 			</div>
 		);
 	}
 }
 
-function show(obj, idButton) {
-	console.log("OBJ: ");
-	console.log("idButton: " + idButton);
-	return {
-      user: {},
-      id: idButton
-    };
-}
 
 export default UserContainer;
