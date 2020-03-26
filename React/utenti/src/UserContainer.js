91a3d0703f42ea839fd982c26114e59fc4daaf03
Padawan04 <Padawan04@DESKTOP-LV5RKMD>
2020-03-26 17:57:13 +0100

    prove e tentativi

--- React/utenti/src/UserContainer.js
+++ React/utenti/src/UserContainer.js
@@ -3,33 +3,41 @@ import ReactDOM from 'react-dom';
 
 // OK ?
 class UserContainer extends React.Component {	
-
+	
 	state = {
-		user: {},
-		id: undefined
+		id: null
+	}
+
+	aggiorna() {
+		this.setState({id: 5});
 	}
 
 	render() {
 		return (
 			<div>
-				<Left name="left" />
-				<Right name="right" value={this.state.id || "1"} />
+				<Left name="left" onClick={this.aggiorna} />
+				<Right name="right" id={this.state.id}/>
 			</div>
 		);
 	}
 }
 
 class Right extends React.Component {
-	
 	state = {
 		user: {},
-		id: null
 	}
 	
-
+	/*constructor(props) {
+		super(props);
+		console.log(this.props.value);
+		this.setState({id: this.props.value})
+	}*/
+	
+	
+	
 	componentWillMount() {
-		if(this.state.id) {
-			fetch('https://jsonplaceholder.typicode.com/users/' + this.state.id)
+		if(this.props.id) {
+			fetch('https://jsonplaceholder.typicode.com/users/' + this.props.id)
 				.then(response => response.json())
 				.then(json => {
 					this.setState({user: json });
@@ -42,7 +50,7 @@ class Right extends React.Component {
 		
 	render() {
 		return(
-			<div>Id: {this.state.user.id} - Nome: {this.state.user.name}</div>
+			<DettaglioUtente user={this.state.user} />
 		);
 
 	}
@@ -52,7 +60,7 @@ class Right extends React.Component {
 class DettaglioUtente extends React.Component {
 	render() {
 		return(
-			<div>{this.props.user}</div>
+			<div>{this.props.user.name}</div>
 		);
 	}
 }
@@ -83,7 +91,7 @@ class Left extends React.Component {
 	render () {
 		var items = this.state.users.map((item, index) => {
 			return (
-				<UserItem key={index} id={item.id} name={item.name} />
+				<UserItem key={index} id={item.id} name={item.name} aggiorna={this.props.aggiona} />
 			);
 		});
 		
@@ -102,6 +110,7 @@ class UserItem extends React.Component {
 	constructor(props) {
 		super(props);
 		this.mostraUser = this.mostraUser.bind(this);
+		console.log("Aggiorna: " + this.props.aggiorna);
 	}
 			
 	mostraUser(event) {
@@ -111,10 +120,15 @@ class UserItem extends React.Component {
 	render () {
 		return(
 			<div>
-				<button type="button" id={this.props.id} onClick={this.mostraUser}>Id: {this.props.id}, Name: {this.props.name}, prova: {this.props.id}</button>
+				<button type="button" id={this.props.id} onClick={this.props.aggiona}>Id: {this.props.id}, Name: {this.props.name}, prova: {this.props.id}</button>
 			</div>
 		);
 	}
+}
+
+
+function getId(id) {
+	return id;
 }
 
 
