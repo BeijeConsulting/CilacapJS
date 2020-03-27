aab84a68069603916540f1305241efca3ece3c89
Padawan11 <Padawan11@DESKTOP-MQ1AMB6.lan>
2020-03-27 11:25:31 +0100

    Aggiunta funzione getUser() e visualizzazione dei dati del singolo utente

--- React/reactapp/src/UsersList.js
+++ React/reactapp/src/UsersList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import User from './User.js';
+import './UsersList.css';
 
 
 export default class UsersList extends React.Component {
@@ -8,14 +8,15 @@ export default class UsersList extends React.Component {
     render() {
 
         return (
-            <div id="UsersList" className="component-users-list">
-                    {getUsers()}
+            <div id="UsersList"> 
+                    {getAllUsers()}
             </div>
+            
         );
     }
 }
 
-function getUsers() {
+function getAllUsers() {
         let names = []; 
 
         fetch('https://jsonplaceholder.typicode.com/users')
@@ -26,11 +27,52 @@ function getUsers() {
                 let usersLength = Object.keys(data).length;
                 for (var i = 0; i < usersLength; i++) {
                     names.push(data[i]);               
-            }
-            let namesList = names.map(name => (
-                <User key={name.name} id={name.id} name={name.name}></User>
-              ));
+                }
+            
+                let namesList = names.map(name => (
+                    <div  className="User"
+                        key={name.name} 
+                        id={name.id} 
+                        name={name.name}
+                        onClick={ () => getUser(name) }
+                    >
+                    User {name.name}
+                    </div>
+                ));
+                let namesList2 = (
+                <div>
+                    <div className="UsersList">
+                        {namesList}
+                    </div>
+                    <div id="VisualUsers" className="VisualUsers">
+
+                    </div>
+                </div>);
+                
               
-            ReactDOM.render(namesList, document.getElementById('UsersList'));
+            ReactDOM.render(namesList2, document.getElementById('UsersList'));
     })
 }
+
+function getUser(name) {
+    console.log(name.address);
+    console.log(name.name);
+
+    let UserInfo = (
+    <div>
+        <p>Name: {name.name}</p>
+        <p>Username: {name.username}</p>
+        <p>Email: {name.email}</p>
+        <p>Phone: {name.phone}</p>
+        <div>Address: </div>
+            <p> &emsp; Street: {name.address.street}</p>
+            <p> &emsp; Suite:  {name.address.suite}</p>
+            <p> &emsp; City:  {name.address.city}</p>
+            <p> &emsp; Zipcode:  {name.address.zipcode}</p>
+        <div>Company: </div>
+            <p> &emsp; Name: {name.company.name}</p>
+            <p> &emsp; CatchPhrase: {name.company.catchPhrase}</p>
+    </div>
+        );
+        ReactDOM.render(UserInfo, document.getElementById('VisualUsers'));
+    }
